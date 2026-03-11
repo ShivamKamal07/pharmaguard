db28a257f3b8adddaed05784e0eb61bbac7622f4
kamal <shivamkamal.9936@gmail.com>
2026-03-11 17:44:30 +0530

    authentication added

--- Backend/app.js
+++ Backend/app.js
@@ -1,8 +1,7 @@
-// app.js
 const express = require("express");
 const cors = require("cors");
 
-
+const authRoutes = require("./routes/authRoutes");
 const analysisRoutes = require("./routes/analysisRoutes");
 
 const app = express();
@@ -12,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 // Routes
+app.use("/api/auth", authRoutes);
 app.use("/api", analysisRoutes);
 
 module.exports = app;
--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,7 @@ const app = require("./app");
 
 
 
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch((err) => console.log(err));
