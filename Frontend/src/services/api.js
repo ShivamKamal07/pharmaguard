131362f737f93a2dbbd526f98d95555bb5bb5b80
kamal <shivamkamal.9936@gmail.com>
2026-04-26 00:39:56 +0530

    update file

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://pharmaguard-1-8aia.onrender.com",
 });
 
 // Attach token automatically
