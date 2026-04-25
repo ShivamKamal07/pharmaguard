2281ab83127a94b11d410ea06e1d9f508b4f58a3
SHIVAM <shivamkamal.9936@gmail.com>
2026-04-26 00:13:03 +0530

    Change API base URL to production endpoint

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://pharmaguard-ks5i.onrender.com",
 });
 
 // Attach token automatically
